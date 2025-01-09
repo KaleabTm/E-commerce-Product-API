f3a598f059971fb5eb781f82e7d4666491eb5348
KaleabTm <kaltamiru8@gmail.com>
2025-01-10 01:52:33 +0300

    minor update

--- users/signals.py
+++ users/signals.py
@@ -1,12 +1,11 @@
-# users/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from carts.models import Cart
-from django.contrib.auth import get_user_model  # Import this
+from django.contrib.auth import get_user_model  
 
-User = get_user_model()  # Get the actual user model
+User = get_user_model()  
 
-@receiver(post_save, sender=User)  # Use User model directly here
+@receiver(post_save, sender=User)  
 def create_user_cart(sender, instance, created, **kwargs):
     if created:
         Cart.objects.create(user=instance)
